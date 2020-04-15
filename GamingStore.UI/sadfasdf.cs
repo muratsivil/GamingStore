f0cbfb2224ed3cba374ad61acb0997829ba6afd6
Murat Sivil <63340228+muratsivil@users.noreply.github.com>
2020-04-15 12:39:16 +0300

    New Changes

---
+++ GamingStore.UI/sadfasdf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamingStore.UI
+{
+    public partial class sadfasdf : Form
+    {
+        public sadfasdf()
+        {
+            InitializeComponent();
+        }
+    }
+}
